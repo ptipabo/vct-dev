9a774445321597ac70eb2293446d93f7390dc06b
Thibaut Van Callemont <vancallemont.thibaut@gmail.com>
2023-10-03 22:57:59 +0200

    Creation of the footer section and the admin tool bar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(string $locale = 'fr'): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'locale' => $locale,
         ]);
+    }
+
+    #[Route('/{locale}', name: 'app_home_lang')]
+    public function indexLang(): Response
+    {
+        $locale = 'fr';
+
+        return $this->index($locale);
     }
 }
